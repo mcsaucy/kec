08bbce41cc6ba44b598c002d453aa1bf97beac21
Josh McSavaney <mcsaucy@csh.rit.edu>
2020-04-28 22:29:27 -0400

    Add a check and messaging for still-booting nodes.

--- ssh_node0.sh
+++ ssh_node0.sh
@@ -7,5 +7,11 @@ DOMAIN_ID="$(echo "${OUR_VMS[0]}" | awk '{print $1}')"
 IP=$(virsh --connect qemu:///system domifaddr "$DOMAIN_ID" \
     | grep "vnet" | awk '{print $4}' | cut -d/ -f1)
 
+if [[ -z "$IP" ]]; then
+    echo "Can't determine IP of node 0. This could be because it's booting."
+    echo "Ensure it's running and try again later."
+    exit 1
+fi
+
 echo "Preparing to ssh to core@$IP"
 ssh "core@$IP"
