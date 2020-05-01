84b48c7589a661d8b39b42b6133cb33972e909a9
Josh McSavaney <mcsaucy@csh.rit.edu>
2020-05-01 19:37:36 -0400

    fix a lingering issue typo reference in ssh_node0

--- ssh_node0.sh
+++ ssh_node0.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mapfile OUR_VMS < <(virsh --connect qemu:///system list | grep issue_307_node0)
+mapfile OUR_VMS < <(virsh --connect qemu:///system list | grep issue_329_node0)
 DOMAIN_ID="$(echo "${OUR_VMS[0]}" | awk '{print $1}')"
 IP=$(virsh --connect qemu:///system domifaddr "$DOMAIN_ID" \
     | grep "vnet" | awk '{print $4}' | cut -d/ -f1)
